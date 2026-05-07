c0de0cbb1160f0a0c30cc13a03d7d8859789444b
David G. <85518858+dgalanberasaluce@users.noreply.github.com>
2026-05-07 22:51:23 +0200

    remove homelab docs from main page

--- run_local.sh
+++ run_local.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 echo "Cleaning up previous setup..."
-rm -rf home/_site
+rm -rf ./_site
 
 echo "Building Jekyll (Main)..."
-cd home
 bundle exec jekyll build
-cd ..
-
-echo "Building MkDocs (HomeLab)..."
-cd homelab-docs
-mkdocs build --site-dir ../home/_site/homelab
-cd ..
 
 echo "Site built"
 echo "Initiating server at http://localhost:8080"
 
-cd home/_site
-python3 -m http.server 8080
+cd ./_site
+python3 -m http.server 8000
